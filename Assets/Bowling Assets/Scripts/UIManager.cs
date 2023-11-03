137bcc66450dff7c5f67df5726a0398a5d15d6b6
Justin Derot <justinderot@hotmail.ca>
2023-11-02 20:59:08 -0700

    Lesson 10 Commit

--- Assets/Bowling Assets/Scripts/UIManager.cs
+++ Assets/Bowling Assets/Scripts/UIManager.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private Transform frameHolder;
+    [SerializeField] private GameObject messageUIStrike;
+    [SerializeField] private GameObject messageUISpare;
+    [SerializeField] private GameObject gameOverUI;
+    [SerializeField] private TMP_Text scoreText;
+
+    private FrameUI[] frames;
     // Start is called before the first frame update
     void Start()
     {
